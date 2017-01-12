c8000618b0dc458f9a43150455780596c089091b
Jose Rivera <jrivera@newrelic.com>
2017-01-11 20:39:04 -0600

    Initial Commit

---
+++ WBS Dashboard/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WBS Dashboard
+//
+//  Created by Jose Rivera on 1/11/17.
+//  Copyright © 2017 Qixi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
